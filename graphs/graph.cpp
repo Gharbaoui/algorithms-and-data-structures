e0aba2e8faf5084e6d417dc2776856edde2f1128
Gharbaoui <mohamed9gharbaoui.com>
2022-01-04 08:32:47 +0000

    calling traversal in main thats all

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -4,12 +4,16 @@
 int main()
 {
 	Graph g;
+	int num_of_edges;
 	int num_of_v;
 	int x, y;
+	std::cout << "please enter number of edges: ";
+	std::cin >> num_of_edges;
 	std::cout << "please enter number of vertices: ";
 	std::cin >> num_of_v;
+	g.setNumberOfVerteces(num_of_v);
 
-	for (int i = 0; i < num_of_v; ++i)
+	for (int i = 0; i < num_of_edges; ++i)
 	{
 		std::cout << "Enter a node and what is connecting to: ";
 		std::cin >> x;
@@ -17,6 +21,5 @@ int main()
 		g.insert(x, y);
 	}
 
-
-	g.display();
+	g.dfs_trvaresal(0);
 }
