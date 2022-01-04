ad5f1908c67268d52512d1c8f275765fb9029005
Gharbaoui <mohamed9gharbaoui.com>
2022-01-04 08:32:15 +0000

    exploring graph traversal specially dfs (depth first search) recursive and iter

--- graphs/graph.hpp
+++ graphs/graph.hpp
@@ -4,7 +4,7 @@
 #define MX_EDGES 100
 
 #include <iostream>
-
+#include <stack>
 
 //	[0]-----[1]----[2]
 //	| ______| |	|
@@ -42,6 +42,8 @@ class Graph
 {
 	Edge	*edges[MX_EDGES + 1]; // +1 in my case i will insert edges usually start from 1
 	int	connect_edges[MX_EDGES + 1]; // this will tell me how many edges are connect to each one of my edges
+	int	nverteces;
+	bool	visited[MX_EDGES + 1];
 	
 	public:
 		Graph()
@@ -51,6 +53,7 @@ class Graph
 				edges[i] = NULL;
 				connect_edges[i] = 0;
 			}
+			nverteces = 0;
 		}
 
 		// function will accept two ids of edge
@@ -70,28 +73,68 @@ class Graph
 			++connect_edges[id1];
 		}
 
+		void	setNumberOfVerteces(int n)
+		{
+			nverteces = n;
+		}
 
-
-
-		void	display() const
+		void	dfs_trvaresal(int start)
 		{
 			for (int i = 0; i < MX_EDGES + 1; ++i)
-				if (edges[i])
-					display_row(i, edges[i]);
+				visited[i] = false;
+			std::cout << "recursion\n";
+			pr_dfs_rec(start);
+			std::cout << std::endl;
+
+			std::cout << "iteration\n";
+			for (int i = 0; i < MX_EDGES + 1; ++i)
+				visited[i] = false;
+			pr_dfs_iter(start);
+			std::cout << std::endl;
+		}
+	private:
+		void	pr_dfs_rec(int start)
+		{
+			Edge *tmp = edges[start];
+			if (!visited[start])
+			{
+				std::cout << start << " ";
+				visited[start] = true;
+			}
+			
+			while (tmp)
+			{
+				if (!visited[tmp->getId()])
+					pr_dfs_rec(tmp->getId());
+				tmp = tmp->getNext();
+			}
+		}
+		void	pr_dfs_iter(int start)
+		{
+			std::stack<int> edges_to_visit;
+			Edge *tmp;
+			int cur;
+			edges_to_visit.push(start);
+			while (!edges_to_visit.empty())
+			{
+				cur  = edges_to_visit.top();
+				edges_to_visit.pop();
+				if (!visited[cur])
+				{
+					std::cout << cur << " ";
+					visited[cur] = true;
+				}
+				tmp = edges[cur];
+				while (tmp)
+				{
+					if (!visited[tmp->getId()])
+						edges_to_visit.push(tmp->getId());
+					tmp = tmp->getNext();
+				}
+			}
 		}
 
-		void	display_row(int index, Edge *start_node) const
-		{
-			std::cout << "| " << index << " | ";
-			while (start_node)
-			{
-				std::cout << "( " << start_node->getId() << " ) ";
-				if (start_node->getNext())
-					std::cout << "-> ";
-				start_node = start_node->getNext();
-			}
-			std::cout << "[" << connect_edges[index] << "]" << std::endl; 
-		}
+
 };
 
 
