4c64e62171a24be645d6f55263315cb2734c325d
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-03-13 08:15:22 +0100

    bellmford applied to cheapest k filght in leetcode

--- algorithms-book/weighted/Cheapest-Flights-Within-K-Stops/main.cpp
+++ algorithms-book/weighted/Cheapest-Flights-Within-K-Stops/main.cpp
@@ -3,65 +3,51 @@
 #include <limits>
 
 /**/
-
-
 class Solution {
 	public:
+		Solution() {
+			result = {max, max};
+			max = std::numeric_limits<int>::max();
+		}
 		int findCheapestPrice(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k) {
-			std::vector<int>	stops(n, max);
-			std::vector<int>	tmpStops(n, -1);
-			std::vector<int>	dists(n, max);
-			std::vector<int>	real_dists(n, max);
-			dists[src] = 0;
-			stops[src] = 0;
-			stops[dst] = -1;
-			tmpStops[src] = 0;
-			bool changed;
-			int tmpdist;
+			costs.resize(n, max);
+			tmp_costs.resize(n, max);
 
-			++k;
-			for (size_t i = 0; i < n; ++i)
+			costs[src] = 0;
+
+			for (i = 0; i <= k; ++i)
 			{
-				changed = false;
-				for (auto &E : flights)
+				for (auto &edge : flights)
 				{
-					if (dists[E[0]] != max)
+					if (costs[edge[0]] != max)
 					{
-						// means that we know what it takes to get to the start of this edge
-						tmpdist = dists[E[0]] + E[2];
-						if (tmpdist < dists[E[1]] && tmpStops[E[0]] < k)
-						{
-							dists[E[1]] = tmpdist;
-							tmpStops[E[1]] = tmpStops[E[0]] + 1;
-							changed = true;
-						}
+						tmp_cost = costs[edge[0]] + edge[2];
+						if (tmp_cost < tmp_costs[edge[1]])
+							tmp_costs[edge[1]] = tmp_cost;
 					}
-				}
-				
-				if (changed)
-				{
-					if (tmpStops[dst] <= k){
-						stops = tmpStops;
-						real_dists = dists;
-					}
-				}else
-					break ;
-			}
-			if (stops[dst] != -1)
-			{
-				std::cout << stops[dst] << "\n";
-				return real_dists[dst];
-			}
+                }
+			    costs = tmp_costs;
+
+            }
+			if (costs[dst] != max)
+				return costs[dst];
 			return -1;
 		}
 	private:
-		int max = std::numeric_limits<int>::max();
+		std::vector<int>	costs, tmp_costs;
+		int	*cur;
+		unsigned short i;
+		unsigned short count;
+		unsigned short tmp_cost;
+		std::pair<unsigned short, unsigned short> result;
+		int max;
 };
+
 
 int main()
 {
 	Solution s;
-	std::vector<std::vector<int>> graph{{0,1,1},{0,2,5},{1,2,1},{2,3,1}};
+	std::vector<std::vector<int>> graph {{0,3,7},{4,5,3},{6,4,8},{2,0,10},{6,5,6},{1,2,2},{2,5,9},{2,6,8},{3,6,3},{4,0,10},{4,6,8},{5,2,6},{1,4,3},{4,1,6},{0,5,10},{3,1,5},{4,3,1},{5,4,10},{0,1,6}};
 
-	std::cout << s.findCheapestPrice(4, graph, 0, 3, 1) << "\n";
+	std::cout << s.findCheapestPrice(7, graph, 2, 4, 1) << "\n";
 }
