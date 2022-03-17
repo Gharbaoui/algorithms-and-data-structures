9387146646c61d67c4186cd2bd0f8779a487c84d
Gharbaoui <mohamed9gharbaoui.com>
2022-03-17 05:13:01 +0000

    insertion sort

--- insertion-sort/insert-sort.cpp
+++ insertion-sort/insert-sort.cpp
@@ -27,48 +27,10 @@ struct Gen
 
 void	insertion_sort(std::vector<int> &A, int size)
 {
-	int smv;
-	int smi;
-	for (int i = 0; i <= size - 2; ++i)
-	{
-		smv = A[i];
-		smi = i;
-		for (int j = i + 1; j <= size - 1; ++j)
-		{
-			if (smv > A[j])
-			{
-				smv = A[j];
-				smi = j;
-			}
-		}
-		std::swap(A[i], A[smi]);
-	}
+	for (int i = 0; i < size; ++i)
+		for (int j = i + 1; (j > 0 && A[j] < A[j - 1]) ; --j)
+			std::swap(A[j], A[j - 1]);
 }
-
-/*
- 	complexity
-	outer loop will run n times where n = size - 2
-	stuff inside it at lines 34 35 44 are constants O(1)
-	and the loop that dependent on i
-	inner loop will run at (n - j) times ignoring 1 just for simplfication
-	at first will run (n + O(1)) times
-	at second will run (n - 1 + O(1)) times
-	at third will run (n - 2 + O(1)) times
-	.
-	.
-	.
-	at end will run (0 + O(1))
-	we could write total will be
-	(n + O(1)) + (n - 1 + O(1)) + . . . O(1)
-	reorgnize
-	(0 + 1 + 2 + 3 + . . . n) + ((O(1) + O(1) + O(1) + ...)n times)
-	first part will be 
-	n*(n-1)/2
-	second part will be 
-	n*O(1)
-	n^2 + n
-	so O(n^2) maybe i skip some steps unexplained
- */
 
 int main()
 {
