0a94748b0d43270c56be708d947bf598f24fe3fe
Gharbaoui <mohamed9gharbaoui.com>
2022-03-20 05:17:05 +0000

    change a little bit

--- merg-sort/main.cpp
+++ merg-sort/main.cpp
@@ -2,8 +2,46 @@
 #include <vector>
 
 
-void	merg_sort(std::vector<int> &arr, int start, int end);
-void	merg_sorted(std::vector<int> &v, int start, int mid, int end);
+void combine(int start, int mid, int end, std::vector<int> &A)
+{
+	int TMP[end - start + 1];
+	int k = 0;
+	int i, j;
+	for (i = start, j = mid + 1; (i <= mid && j <= end);)
+	{
+		if (A[i] < A[j])
+		{
+			TMP[k] = A[i];
+			++i;
+		}
+		else
+		{
+			TMP[k] = A[j];
+			++j;
+		}
+		++k;
+	}
+	while (j <= end)
+		TMP[k++] = A[j++];
+	while (i <= mid)
+		TMP[k++] = A[i++];
+	k = 0;
+	for(i = start; i <= end; ++i, ++k)
+		A[i] = TMP[k];
+}
+
+void merg_sort(int start, int end, std::vector<int> &A)
+{
+	int mid;
+	if (end - start > 0)
+	{
+		mid = start + (end - start)/2;
+		merg_sort(start, mid, A);
+		merg_sort(mid+1, end, A);
+		combine(start, mid, end, A);
+	}
+}
+
 int main()
 {
 	int size;
@@ -19,49 +57,8 @@ int main()
 	}
 	std::cout << std::endl;
 
-	merg_sort(v, 0, size - 1);
+	merg_sort(0, size - 1, v);
 	for (int i = 0; i < size; ++i)
 		std::cout << v[i] << ", ";
 	std::cout << std::endl;
-}
-
-void	merg_sort(std::vector<int> &v, int start, int end)
-{
-	int mid;
-	if (end > start)
-	{
-		mid = start + (end - start)/2;
-		merg_sort(v, 0, mid);
-		merg_sort(v, mid+1, end);
-		merg_sorted(v, start, mid, end);
-	}
-}
-
-void	merg_sorted(std::vector<int> &v, int start, int mid, int end)
-{
-	std::vector<int> tmp(end - start + 1);
-	int	k = 0;
-	int	i;
-	int	j;
-
-	for (i = start, j = mid + 1; i <= mid && j <= end;)
-	{
-		if (v[i] < v[j])
-		{
-			tmp[k] = v[i];
-			++i;
-		}
-		else
-		{
-			tmp[k] = v[j];
-			++j;
-		}
-		++k;
-	}
-	while (i <= mid)
-		tmp[k++] = v[i++];
-	while (j <= end)
-		tmp[k++] = v[j++];
-	for (int i = 0; start + i<= end; ++i)
-		v[start + i] = tmp[i];
 }
